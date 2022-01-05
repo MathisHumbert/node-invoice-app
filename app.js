52d90dd18ed36c9e25e3e8ac90a2338c4464b0e5
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-05 13:27:19 +0100

    invoice controllers is done

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/api/v1/invoices', invoicesRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const start = async () => {
   try {
--- controllers/invoices.js
+++ controllers/invoices.js
@@ -1,21 +1,51 @@
+const Invoice = require('../models/Invoice');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
+
 const getAllInvoices = async (req, res) => {
-  res.send('get all invoices');
+  // createdBy
+  const invoices = await Invoice.find({});
+  res.status(StatusCodes.OK).json(invoices);
 };
 
 const getInvoice = async (req, res) => {
-  res.send('get invoice');
+  const invoiceID = req.params.id;
+
+  // createdBy
+  const invoice = await Invoice.findOne({ _id: invoiceID });
+  if (!invoice) {
+    throw new NotFoundError(`No invoice with the id ${invoiceID}`);
+  }
+  res.status(StatusCodes.OK).json(invoice);
 };
 
 const createInvoice = async (req, res) => {
-  res.send('create invoice');
+  // createdBy
+  const invoice = await Invoice.create(req.body);
+  res.status(StatusCodes.CREATED).json(invoice);
 };
 
 const updateInvoice = async (req, res) => {
-  res.send('update invoice');
+  // createdBy
+  const invoiceID = req.params.id;
+  const invoice = await Invoice.findOneAndUpdate({ _id: invoiceID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!invoice) {
+    throw new NotFoundError(`No invoice with the id ${invoiceID}`);
+  }
+  res.status(StatusCodes.OK).json(invoice);
 };
 
 const deleteInvoice = async (req, res) => {
-  res.send('delete invoice');
+  // createdBy
+  const invoiceID = req.params.id;
+  const invoice = await Invoice.findByIdAndRemove({ _id: invoiceID });
+  if (!invoice) {
+    throw new NotFoundError(`No invoice with id ${invoiceID}`);
+  }
+  res.status(StatusCodes.OK).send('Invoice removed');
 };
 
 module.exports = {
